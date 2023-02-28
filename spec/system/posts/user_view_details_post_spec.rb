80dd9bf973f579f3de60bb45a498a531b484fe12
Anita Luri <anitaluri.h@gmail.com>
2023-02-28 18:22:39 -0300

    ajuste home

--- spec/system/posts/user_view_details_post_spec.rb
+++ spec/system/posts/user_view_details_post_spec.rb
@@ -25,4 +25,32 @@ describe 'Usuário acessa página de uma postagem especifica' do
     expect(page).to have_content post_c.restaurant_tip
     expect(page).to have_content post_c.tour_tip
   end
+  it 'a partir da home' do
+    praia = Category.create!(name: 'Praia')
+    cidade = Category.create!(name: 'Cidade')
+    post_a = Post.create!(title: 'Big Sur - Rota 1', country: 'USA', state: 'CA', city: 'Big Sur',
+                          category: praia, text: 'Big Sur é uma região localizada no centro da
+                          Califórnia, nos Estados Unidos. ')
+    image_a_path = Rails.root.join('spec/support/images/ca.jpg')
+    post_a.image.attach(io: image_a_path.open, filename: 'ca.jpg')
+    post_b = Post.create!(title: 'Nova York', country: 'USA', state: 'NY', city: 'Nova Iorque',
+                          category: cidade, text: 'Nova Iorque ou Nova York é um dos 50 estados
+                          dos Estados Unidos, localizado na Região nordeste do país.')
+    image_b_path = Rails.root.join('spec/support/images/ny.jpg')
+    post_b.image.attach(io: image_b_path.open, filename: 'ny.jpg')
+    post_c = Post.create!(title: 'Paraty', country: 'BRA', state: 'RJ', city: 'Paraty', category: praia,
+                          text: 'Paraty fica na costa sudeste do Brasil, 200 quilômetros ao sul do Rio')
+    image_c_path = Rails.root.join('spec/support/images/paraty.jpg')
+    post_c.image.attach(io: image_c_path.open, filename: 'paraty.jpg')
+
+    visit root_path
+    click_on 'Paraty'
+
+    expect(page).to have_content 'Paraty'
+    expect(page).to have_content 'RJ - Paraty'
+    expect(page).to have_content 'Praia'
+    expect(page).to have_content post_c.text
+    expect(page).to have_content post_c.restaurant_tip
+    expect(page).to have_content post_c.tour_tip
+  end
 end
