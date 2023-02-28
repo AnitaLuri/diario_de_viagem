69100a5463e90c88a2605e7551f107f87236ad80
Anita Luri <anitaluri.h@gmail.com>
2023-02-28 17:48:53 -0300

    usuário visualiza link de outros projetos

--- spec/system/about/user_view_about_spec.rb
+++ spec/system/about/user_view_about_spec.rb
@@ -7,14 +7,14 @@ describe 'Usuário vê página sobre' do
 
     expect(page).to have_content 'Sobre quem desenvolveu'
     expect(page).to have_css 'img[src*="eu"]'
-    expect(page).to have_link 'github'
-    expect(page).to have_link 'LinkedIn'
-    expect(page).to have_link 'Instagram'
-    expect(page).to have_content 'Outros projetos'
-    expect(page).to have_link 'TreinaDev - Insurance'
+    expect(page).to have_content 'Outros Projetos'
+    expect(page).to have_link 'TreinaDev'
+    expect(page).to have_css 'img[src*="insurance"]'
     expect(page).to have_link 'Sistema de Frete'
+    expect(page).to have_css 'img[src*="frete"]'
     expect(page).to have_link 'Conversor'
+    expect(page).to have_css 'img[src*="convert"]'
     expect(page).to have_link 'Ranking'
-    expect(page).to have_link 'Projeto-SQL'
+    expect(page).to have_css 'img[src*="ranking"]'
   end
 end
